4aa35faf472ccf8b0134aebc724cc5e1795d2165
julio hernandez <julioandres72@gmail.com>
2020-08-23 16:26:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,5 +37,8 @@ def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
   index = input_to_index(num)
+  if  valid_move?(board,index) == true
+    move(board,index)
+    display_board(board)
 
 end
