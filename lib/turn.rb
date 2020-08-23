00210d7ad4831dc94ed0363a709908ac40231607
julio hernandez <julioandres72@gmail.com>
2020-08-23 15:43:23 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+		true
+	else
+		false
+	end
+end
+
+
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == "" || board[position] == nil
+  	false
+
+  else
+  	true
+
+  end
+
+end
